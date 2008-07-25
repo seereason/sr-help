21cb770e1d589da8142114d38c88a411ddbd7d07
jeremy <jeremy@n-heptane.com>
2008-07-25 23:47:31 +0000

    moved out of extra, fixed example

---
+++ src/Text/Help.hs
@@ -0,0 +1,18 @@
+module Text.Help 
+    ( module Text.Help.Man
+    , module Text.Help.DSL
+    , module Text.Help.Doc
+    , module Text.Help.Groff
+    , module Text.Help.Util
+    , module Text.Help.GetOpt
+    , module Text.Help.Markup
+    ) where
+
+import Text.Help.Man
+import Text.Help.DSL
+import Text.Help.Doc
+import Text.Help.Groff
+import Text.Help.Util
+import Text.Help.GetOpt
+import Text.Help.Markup
+
